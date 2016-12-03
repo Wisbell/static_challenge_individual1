84b1e29849a0ead06d61184c538517478f8b6598
Wisbell <westleyisbell@gmail.com>
2016-12-03 12:50:36 -0600

    Captured input elements and add event listeners to change the inputs if either one changed

--- form.js
+++ form.js
@@ -10,3 +10,37 @@ the same values for min, max, and default. When the user changes the
 
 
 console.log("Form.js loaded");
+
+
+// Capture input elements
+
+var number_input_field = document.querySelector('input[name=number_input]');
+
+var range_input_field = document.querySelector('input[name=range_input]');
+
+//console.log(number_input_field);
+
+//console.log(range_input_field);
+
+
+/*-------------------------------*/
+/* Add Event Listeners to inputs */
+/*-------------------------------*/
+
+// https://developer.mozilla.org/en-US/docs/Web/Events/input
+
+// Add event listener to range_input
+
+range_input_field.addEventListener('input', function(){
+    console.log("Range input changed");
+
+    number_input_field.value = range_input_field.value;
+});
+
+
+// Add event listener to number_input
+
+number_input_field.addEventListener('input', function(){
+    console.log("Number input changed");
+    range_input_field.value = number_input_field.value;
+});
